8556ead79436bd5645196dcac8067d76cd3621b1
barab <barab@192.168.0.10>
2022-06-06 22:15:51 -0700

    10.15

--- src/application/AppDriver.java
+++ src/application/AppDriver.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.io.FileNotFoundException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
@@ -22,10 +23,28 @@ public class AppDriver
 		
 		//String a[] = {"-fpolyfor1.txt", "-tv", "-sb"};
 		//new SortManager(a);
+
+		
+		AppManager app = new AppManager();
+		
+		
+		try
+		{
+			app.loadDataFiles("C:\\Users\\barab\\Desktop\\School Files\\SPRING 2022\\311 Advance OOP\\Assignments\\A1 Complexity and Sorting\\1\\Data_Files_ClassName (1)\\polyfor1.txt");
+		} catch (FileNotFoundException e)
+		{
+			System.out.println(e.getMessage());
+		}
+		
+	}	
+	
+	
+	private void testSort() {
 		
 		
 		Cylinder[] list =new Cylinder[6];
 
+		
 		list[0]= new Cylinder(6,10.25);
 		list[1]= new Cylinder(4,21.25);
 		list[2]= new Cylinder(2,54.25);
@@ -40,13 +59,18 @@ public class AppDriver
 		
 		System.out.println("HEAP SORT: ");
 		BaseAreaCompare ba = new BaseAreaCompare();
-		//Arrays.sort(list, gc);
+		Arrays.sort(list, ba);
+
+		long start = System.currentTimeMillis();
+		
 		HeapSort.sort(list, ba);
+		long end= System.currentTimeMillis();
+		long time = end-start;
+		System.out.println("Time is " + time);
 		
 		for( Cylinder s : list ) 
 		{
 			System.out.println("\t" + s.getHeight() + " " + s.getBaseArea()); 
 		}
-		
-	}	
+	}
 }
--- src/managers/AppManager.java
+++ src/managers/AppManager.java
@@ -1,9 +1,27 @@
 package managers;
 
+import java.awt.font.ShapeGraphicAttribute;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Method;
+import java.util.Iterator;
+import java.util.Scanner;
+
 import exceptions.InvalidArgumentException;
+import problemdomain.Cylinder;
+import problemdomain.Shape;
 
 public class AppManager
 {
+
+	private final String CYLINDER = "Cylinder";
+	private final String CONE = "Cone";
+	private final String PYRAMID = "Pyramid";
+	private final String SQUAREPRISM = "SquarePrism";
+	private final String TRIANGULARPRISM = "TriangularPrism";
+	private final String PENTAGONALPRISM = "PentagonalPrism";
+	private final String OCTAGONALPRISM = "OctagonalPrism";
+
 	private final String SORT = "-s";
 	private final String COMPARE = "-t";
 	private final String FILENAME = "-f";
@@ -12,8 +30,13 @@ public class AppManager
 	private String compareType = "";
 	private String filename = "";
 
+	public AppManager()
+	{
+	}
+
 	public AppManager(String args[])
 	{
+
 		try
 		{
 			for (String arg : args)
@@ -47,8 +70,11 @@ public class AppManager
 			{
 				throw new InvalidArgumentException("(-t)Compare type is not defined.");
 			}
+			
+			
+			loadDataFiles(filename);
 
-		} catch (InvalidArgumentException e)
+		} catch (InvalidArgumentException | FileNotFoundException e)
 		{
 			System.out.println(e.getMessage());
 			return;
@@ -56,7 +82,7 @@ public class AppManager
 
 		// Do Sorting
 
-		//test
+		// test
 		System.out.println("File name :" + this.filename);
 		System.out.println("Sort type :" + this.sortType);
 		System.out.println("Compare type :" + this.compareType);
@@ -84,7 +110,7 @@ public class AppManager
 	{
 		if (!(type.equals("b") || type.equals("s") || type.equals("m") || type.equals("q") || type.equals("z")))
 		{
-			throw new InvalidArgumentException(String.format("Sort type (%s) unknown. Please select (b)Bubble, (s)Selection, (m)Merge, (q)Quick or (z)Custom.", type));
+			throw new InvalidArgumentException(String.format("Sort type (%s) unknown. Please select (b)Bubble, (s)Selection, (m)Merge, (q)Quick or (h)Heap.", type));
 		}
 	}
 
@@ -107,6 +133,103 @@ public class AppManager
 		default:
 			return "";
 		}
+	}
+
+	public void loadDataFiles(String path) throws FileNotFoundException
+	{
+		try
+		{
+			File refactor = new File(path);
+			Scanner myReader = new Scanner(refactor);
+	
+			long start = System.currentTimeMillis();
+
+			Shape s = new Cylinder();
+			System.out.println("CLASS NAME : " + s.getClass());
+			
+			
+			// parse text to array
+			String arr[] = myReader.nextLine().split(" ");
+			
+			Shape[] shapes = generateShapesList(arr);
+			System.out.println("array Size : " + shapes.length);
+			
+			for (int i = 0; i < shapes.length; i++)
+			{
+				if(shapes[i]!=null) {
+					String name = shapes[i].getClass().getSimpleName();
+					switch (name)
+					{
+					case CYLINDER:						
+						Method m = shapes[i].getClass().getMethod("toString");
+						System.out.println(m.invoke(shapes[i]));
+						break;
+					case CONE:
+						break;
+					case PYRAMID:
+						break;
+					case SQUAREPRISM:
+						break;
+					case TRIANGULARPRISM:
+						break;
+					case PENTAGONALPRISM:
+						break;
+					case OCTAGONALPRISM:
+						break;
+					}
+					
+					
+				}else {
+					i=shapes.length;
+				}
+				
+			}
+
+			long stop = System.currentTimeMillis();
+			System.out.println("Time: " + (stop - start));
+			myReader.close();
+
+		} catch (Exception e)
+		{
+			throw new FileNotFoundException();
+		}
+	}
+
+	private Shape[] generateShapesList(String[] array)
+	{
+		Shape shapes[] = new Shape[Integer.parseInt(array[0])];
+		int index = 0;
+
+		for (int i = 1; i < array.length; i++)
+		{
+			Shape shape = null;
+			switch (array[i])
+			{
+			case CYLINDER:
+				shape = new Cylinder(Double.parseDouble(array[++i]), Double.parseDouble(array[++i]));
+				break;
+			case CONE:
+				break;
+			case PYRAMID:
+				break;
+			case SQUAREPRISM:
+				break;
+			case TRIANGULARPRISM:
+				break;
+			case PENTAGONALPRISM:
+				break;
+			case OCTAGONALPRISM:
+				break;
+			}
+
+			
+			if (shape != null)
+				shapes[index++] = shape;
+			  
+			 
+		}
+
+		return shapes;
 	}
 
 }
